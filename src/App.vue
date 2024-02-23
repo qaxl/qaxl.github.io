146ad1d61dd0bce2d562750012efdd7c11988244
qaxl <0xfreebuff@gmail.com>
2024-02-23 15:12:43 +0200

    initial vue rewrite

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import NavItem from './components/NavItem.vue'
+
+// fuck y'all i am enabling this shit
+document.documentElement.classList.add('dark');
+</script>
+
+<template>
+  <header>
+    <h1 class="dark:text-3xl font-bold underline">This website is heavily under construction...</h1>
+
+    <h1 className="dark:text-3xl font-bold underline">Hello world!</h1>
+
+    <NavBar>
+      <NavItem href="/" isActive>Home</NavItem>
+      <NavItem href="/about">About</NavItem>
+    </NavBar>
+
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+
+<template>
+  <div class="about">
+    <button class="bg-blue-500 hover:bg-blue-800 dark:text-white py-2 px-4 rounded-md" @click="counter++">Increase counter {{ counter }}</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+button {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
